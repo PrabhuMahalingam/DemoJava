5eececcbec60399483a5653c59d830bbbcc943c8
prabhu <mahalingam.prabu@gmail.com>
2021-03-14 15:36:39 +0530

    first commit

---
+++ src/org/demo/java/SampleDemoClass.java
@@ -0,0 +1,11 @@
+package org.demo.java;
+
+public class SampleDemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Helo Demo Git Class");
+	}
+
+}
