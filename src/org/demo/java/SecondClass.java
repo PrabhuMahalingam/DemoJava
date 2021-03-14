077d852a9bc24dcdd639f94019c4aab3dc6f78c3
prabhu <mahalingam.prabu@gmail.com>
2021-03-14 20:25:48 +0530

    commit via eclipse

--- src/org/demo/java/SecondClass.java
+++ src/org/demo/java/SecondClass.java
@@ -6,6 +6,7 @@ public class SecondClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello this is second class");
 		System.out.println("Hello this is second class");
+		System.out.println("Hello this is second class");
 
 	}
 
