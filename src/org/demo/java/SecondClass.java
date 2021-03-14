8ecc7bae226d8a69b07319d7d279ec17379cecbe
prabhu <mahalingam.prabu@gmail.com>
2021-03-14 19:32:26 +0530

    Second commit

--- src/org/demo/java/SecondClass.java
+++ src/org/demo/java/SecondClass.java
@@ -5,6 +5,7 @@ public class SecondClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello this is second class");
+		System.out.println("Hello this is second class");
 
 	}
 
